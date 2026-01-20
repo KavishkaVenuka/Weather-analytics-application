7481c98157b4dd74b3c9471e1570c002abec80c3
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-01-20 19:50:51 +0530

    Add the Auth0 login component to the root of the DOM

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider domain="dev-5z13ny3t3ert21qz.us.auth0.com"
+      clientId="Y5y8lJwU7MUHMIKjsqlOmMjFUnOxVs3W"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
