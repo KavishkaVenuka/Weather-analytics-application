d006cec4f55839e9fc2524648e9d85b8bf6a7b11
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-01-21 08:24:43 +0530

    Chnage the no of cities 9 to 11

--- server/src/services/WeatherService.js
+++ server/src/services/WeatherService.js
@@ -7,7 +7,7 @@ class WeatherService {
         this.defaultCityId = 6285792;
 
         // Default Dashboard Cities (Provided by user)
-        this.dashboardCityIds = [1248991, 1241964, 1850147, 2644210, 2988507, 2147714, 4930956, 1796236, 3143244];
+        this.dashboardCityIds = [1248991, 1241964, 1850147, 2644210, 2988507, 2147714, 4930956, 1796236, 3143244, 292223, 1275339];
     }
 
     async getWeatherData(cityId) {
